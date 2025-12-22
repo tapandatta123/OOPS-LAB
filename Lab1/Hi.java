ee74d56bf4931c282e38368c0ea29d2e30f6f745
tapandatta123 <tapan05032008@gmail.com>
2025-12-22 22:39:39 +0530

    Add files via upload

---
+++ Lab1/Hi.java
@@ -0,0 +1,12 @@
+public class conditionals{
+public static void main(string args[]){
+ int a = 1;
+ int b = 3;
+ if a > b{
+  System.out.println(a is greater than b);
+ }
+ else a < b{
+   System.out.println(a is less than b);
+ }
+}
+}
---
+++ Lab1/ifElse.java
@@ -0,0 +1,19 @@
+public class ifElse {
+
+   public static void main(String args[]) {
+      int a = 20;
+      if (a == 10) {
+         System.out.print("value of a is 10");
+      } 
+      else if (a == 20) {
+         System.out.print("value of a is 20");
+      }
+      else if (a == 30) {
+         System.out.print("value of a is 30");
+      }
+      else {
+         System.out.print("this is else statement");
+      }
+
+   }
+}
