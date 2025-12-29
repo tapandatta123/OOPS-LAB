64648029880da568a1ec6b35b0d2b732ab39dc75
tapandatta123 <tapan05032008@gmail.com>
2025-12-29 21:23:59 +0530

    Add files via upload

---
+++ Lab1/datatypes.java
@@ -0,0 +1,24 @@
+class datatypes {
+    public static void main(String args []) {
+
+        byte age = 18;
+        short Students = 120;
+        int totalMarks = 550;
+        long phoneNumber = 9876543210L;
+        float height = 5.6f;
+        double averageScore = 88.75;
+        char grade = 'A';
+        boolean isPassed = true;
+        String studentName = "Puppy";
+
+        System.out.println("Age: " + age);
+        System.out.println("Number of Students: " + Students);
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Height: " + height);
+        System.out.println("Average Score: " + averageScore);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+        System.out.println("Student Name: " + studentName);
+    }
+}
